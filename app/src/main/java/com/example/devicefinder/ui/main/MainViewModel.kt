8e85b5637db1eebb9bdf934d87eba59bbf589ccb
“Lauren <larz2016@gmail.com>
2019-12-16 21:57:16 -0600

    changed naming

--- app/src/main/java/com/example/devicefinder/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/devicefinder/ui/main/MainViewModel.kt
@@ -2,11 +2,6 @@ package com.example.devicefinder.ui.main
 
 import android.content.Context
 import android.telephony.TelephonyManager
-import com.android.volley.Request
-import com.android.volley.Response
-import com.android.volley.VolleyError
-import com.android.volley.toolbox.StringRequest
-import com.android.volley.toolbox.Volley
 
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
@@ -18,22 +13,19 @@ import android.os.Bundle
 import android.os.Looper
 import android.os.Message
 import androidx.core.content.ContextCompat
-import com.example.devicefinder.MainActivity
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
 import java.net.HttpURLConnection
-import java.net.URI
 import java.net.URL
-import java.net.URLEncoder
-import java.util.*
-import java.util.logging.Handler
 import kotlin.collections.HashMap
 
 
 class MainViewModel(
     var context: Context?
 ) : ViewModel() {
+    val ALERT_MESSAGE_KEY: String = "ALERT_MESSAGE_KEY"
+
     val showAlertMessage: MutableLiveData<String> = MutableLiveData()
     val requestPermission: MutableLiveData<String> = MutableLiveData()
 
@@ -42,29 +34,31 @@ class MainViewModel(
 
         override fun handleMessage(inputMessage: Message) {
             val bundle: Bundle = inputMessage.data
-            val key: String = "TEST"
-            val s: String = bundle.getString(key.toString())?: key
-            showAlertMessage.value = s
-            Log.d("HELPER", "I made it here: $s")
+            val s: String = bundle.getString(ALERT_MESSAGE_KEY)?: "Default Alert"
+            //showAlertMessage.value = s
+            Log.d("REGISTER_DEVICE", "$s")
         }
     }
 
 
     fun registerDevice(code: String) {
         Log.d("REGISTER_DEVICE", code)
-        val imei = "1234"//getIMEINumber()
+        val imei = getIMEINumber()
 
         if (imei == null) {
             showAlertMessage.value = "Error: Device IMEI number was unable to be retrieved. Check your permissions and try again."
             return
         }
-        val params = getParams("12345", code)
+        val params = getParams(imei, code)
         doAsync() {
             sendRegistrationPost(params)
         }.execute()
     }
 
     private fun getIMEINumber(): String? {
+        //Testing only
+        return "testImei1234"
+
         val unwrappedContext = context ?: return null
         try{
             val permission = ContextCompat.checkSelfPermission(unwrappedContext, READ_PHONE_STATE)
@@ -99,7 +93,7 @@ class MainViewModel(
     private fun sendRegistrationPost(params: Map<String, String>) {
         Log.d("REGISTER_DEVICE", "Sending Registration with " + params)
         var outputWriter: OutputStreamWriter? = null
-        val url = "https://device-finder/api/v1/register"
+        val url = "https://www.device-finder.com/api/v1/register"
         val msg = handler.obtainMessage()
         val bundle : Bundle = Bundle()
         try {
@@ -111,11 +105,13 @@ class MainViewModel(
                     setRequestProperty("Content-Type", "application/json; charset=utf-8")
                     requestMethod = "POST"
 
+                    Log.d("REGISTER_DEVICE", "Setting writer")
                     doOutput = true
                     outputWriter = OutputStreamWriter(outputStream)
                     outputWriter?.write(JSONObject(params).toString())
                     outputWriter?.flush()
                 }.let {
+                    Log.d("REGISTER_DEVICE", "http url connection")
                     if (it.responseCode >= 200 && it.responseCode < 300) it.inputStream else it.errorStream
                 }.let { streamToRead ->
                     BufferedReader(InputStreamReader(streamToRead)).use {
@@ -134,14 +130,16 @@ class MainViewModel(
             Log.d("REGISTER_DEVICE", urlret)
         } catch (e: Exception) {
 
-            bundle.putString("TEST","Your device could not be registered! Please try again." )
-            msg.data = bundle
+            val messageBundle: Bundle = Bundle()
+            messageBundle.putString(ALERT_MESSAGE_KEY,"Your device could not be registered! Please try again." )
+            msg.data = messageBundle
             handler.handleMessage(msg)
             //showAlertMessage.value = "Your device could not be registered! Please try again." // Possibly have boolean that we can check after the function gets to this point and then based on that set the value??
             Log.d("REGISTER_DEVICE", "Error: " + e.toString()) // Another possiblity is to call a function that is passed boolean that sets the toast
         } finally {
-            bundle.putString("TEST", "Your device has been successfully registered.")
-            msg.data = bundle
+            val messageBundle: Bundle = Bundle()
+            messageBundle.putString(ALERT_MESSAGE_KEY, "Your device has been successfully registered.")
+            msg.data = messageBundle
             handler.handleMessage(msg)
 
             outputWriter?.close()
